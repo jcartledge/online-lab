2f3114072d8e9ec4cfe5dff7938e0616aa6030fe
James Cartledge <jcartledge@gmail.com>
2016-04-24 18:48:49 +1000

    gulp ftp excludes

--- gulpfile.js
+++ gulpfile.js
@@ -8,8 +8,23 @@ gulp.task('stage', function () {
     require('./ftp.json')['stage']
   ));
 
+  const globs = [
+    "**",
+    "!*.log",
+    "!wp-config.php",
+    "!wp-content/advanced-cache.php",
+    "!wp-content/backup-db/",
+    "!wp-content/backups/",
+    "!wp-content/blogs.dir/",
+    "!wp-content/cache/",
+    "!wp-content/upgrade/",
+    "!wp-content/uploads/",
+    "!wp-content/wp-cache-config.php",
+    "!wp-content/plugins/hello.php"
+  ];
+
   const dest = '.';
-  return gulp.src('**', { base: 'wordpress', cwd: 'wordpress', buffer: false })
+  return gulp.src(globs, { base: 'wordpress', cwd: 'wordpress', buffer: false })
     .pipe(conn.newerOrDifferentSize(dest)) // only upload newer files
     .pipe(conn.dest(dest));
 });
