1b715dd8955603476bfba5e6c077d8920051e9e8
James Cartledge <jcartledge@gmail.com>
2016-04-24 16:36:48 +1000

    move wordpress into subdir

---
+++ gulpfile.js
@@ -0,0 +1,15 @@
+const gulp = require('gulp');
+const gulpUtil = require('gulp-util');
+const ftp = require('vinyl-ftp');
+
+gulp.task('stage', function () {
+  const conn = ftp.create(Object.assign(
+    {log: gulpUtil.log},
+    require('./ftp.json')['stage']
+  ));
+
+  const dest = 'test';
+  return gulp.src('**', { base: 'wordpress', cwd: 'wordpress', buffer: false })
+    .pipe(conn.newer(dest)) // only upload newer files
+    .pipe(conn.dest(dest));
+});
