09edde28ec7a15106f380c19783644f0a52b1053
James Cartledge <jcartledge@gmail.com>
2016-04-28 13:14:07 +1000

    generic deploy in gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -2,29 +2,36 @@ const gulp = require('gulp');
 const gulpUtil = require('gulp-util');
 const ftp = require('vinyl-ftp');
 
-gulp.task('stage', function () {
-  const conn = ftp.create(Object.assign(
-    {log: gulpUtil.log},
-    require('./ftp.json')['stage']
-  ));
+function deploy(target) {
+  return function () {
+    const conn = ftp.create(Object.assign(
+          {log: gulpUtil.log},
+          require('./ftp.json')[target]
+          ));
 
-  const globs = [
-    "**",
-    "!*.log",
-    "!wp-config.php",
-    "!wp-content/advanced-cache.php",
-    "!wp-content/backup-db/",
-    "!wp-content/backups/",
-    "!wp-content/blogs.dir/",
-    "!wp-content/cache/",
-    "!wp-content/upgrade/",
-    "!wp-content/uploads/",
-    "!wp-content/wp-cache-config.php",
-    "!wp-content/plugins/hello.php"
-  ];
+    const globs = [
+      "**",
+      "!*.log",
+      "!wp-config.php",
+      "!wp-content/advanced-cache.php",
+      "!wp-content/backup-db/",
+      "!wp-content/backups/",
+      "!wp-content/blogs.dir/",
+      "!wp-content/cache/",
+      "!wp-content/upgrade/",
+      "!wp-content/uploads/",
+      "!wp-content/wp-cache-config.php",
+      "!wp-content/plugins/hello.php"
+    ];
 
-  const dest = '.';
-  return gulp.src(globs, { base: 'wordpress', cwd: 'wordpress', buffer: false })
-    .pipe(conn.newerOrDifferentSize(dest)) // only upload newer files
-    .pipe(conn.dest(dest));
+    const dest = '.';
+    return gulp.src(globs, { base: 'wordpress', cwd: 'wordpress', buffer: false })
+      .pipe(conn.newerOrDifferentSize(dest))
+      .pipe(conn.dest(dest));
+  }
+}
+
+['stage', 'deploy'].map(function (target) {
+  gulp.task(target, deploy(target));
 });
+
